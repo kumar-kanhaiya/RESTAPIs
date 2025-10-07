6595b3fa4cc9c8a63e3292eab1d4f2aa219c373a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-07 15:12:34 +0530

    code

--- LearningRestAPIs/src/main/java/com/api/LearningRestAPIs/controller/StudentController.java
+++ LearningRestAPIs/src/main/java/com/api/LearningRestAPIs/controller/StudentController.java
@@ -2,19 +2,32 @@ package com.api.LearningRestAPIs.controller;
 
 
 import com.api.LearningRestAPIs.dto.StudentDto;
+import com.api.LearningRestAPIs.entity.Student;
+import com.api.LearningRestAPIs.repository.StudentRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 public class StudentController {
 
+    @Autowired
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(4L,"Kanhaiya" , "kanhaiya@gmail.com");
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
     }
 
     @GetMapping("/student/{id}")
---
+++ LearningRestAPIs/src/main/java/com/api/LearningRestAPIs/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.api.LearningRestAPIs.repository;
+
+import com.api.LearningRestAPIs.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+
+    List<Student> getStudentById(Long id);
+}
