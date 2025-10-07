f0ab16ef32c1a8eb001bff21cf854a8e695d0d96
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-07 14:11:55 +0530

    code

--- LearningRestAPIs/src/main/java/com/api/LearningRestAPIs/controller/StudentController.java
+++ LearningRestAPIs/src/main/java/com/api/LearningRestAPIs/controller/StudentController.java
@@ -2,6 +2,9 @@ package com.api.LearningRestAPIs.controller;
 
 
 import com.api.LearningRestAPIs.dto.StudentDto;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,11 @@ public class StudentController {
     public StudentDto getStudentById(){
         return new StudentDto(4L,"krishna","krish@gmail.com");
     }
+
+//    Configuration config = new Configuration().configure().addAnnotatedClass(StudentDto.class);
+//
+//    SessionFactory sessionFactory = config.buildSessionFactory();
+//
+//    Session session = sessionFactory.openSession();
 
 }
