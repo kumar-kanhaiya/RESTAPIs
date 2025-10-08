2812cd9b6b52d2c55d1a858eeb02e00cbe36a09e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-08 11:41:23 +0530

    code

---
+++ LearningRestAPIs/src/main/java/com/api/LearningRestAPIs/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.api.LearningRestAPIs.service;
+
+import com.api.LearningRestAPIs.dto.StudentDto;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<StudentDto> getAllStudent();
+
+
+
+}
---
+++ LearningRestAPIs/src/main/java/com/api/LearningRestAPIs/service/impl/StudentServiceImp.java
@@ -0,0 +1,29 @@
+package com.api.LearningRestAPIs.service.impl;
+
+import com.api.LearningRestAPIs.dto.StudentDto;
+import com.api.LearningRestAPIs.entity.Student;
+import com.api.LearningRestAPIs.repository.StudentRepository;
+import com.api.LearningRestAPIs.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentServiceImp  implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public List<StudentDto> getAllStudent() {
+        List<Student> students = studentRepository.findAll();
+        List<StudentDto> list = students.stream().map(student -> new
+                StudentDto(student.getId(),student.getName() , student.getEmail())).toList();
+        return List.of();
+    }
+
+
+
+}
